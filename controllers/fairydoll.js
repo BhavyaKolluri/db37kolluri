217e4282d18c72dc33e86e3207eb74cc92898187
BhavyaKolluri <89408263+BhavyaKolluri@users.noreply.github.com>
2021-11-18 02:15:25 -0600

    added requirements

--- controllers/fairydoll.js
+++ controllers/fairydoll.js
@@ -113,41 +113,58 @@ exports.fairydoll_view_one_Page = async function(req, res) {
 // Handle building the view for creating a fairydoll.
 // No body, no in path parameter, no query.
 // Does not need to be async
-// exports.fairydoll_create_Page = function(req, res) {
-//     console.log("create view")
-//     try{
-//     res.render('fairydollcreate', {title: 'fairydoll Create'});
-//     }
-//     catch(err){
-//     res.status(500)
-//     res.send(`{'error': '${err}'}`);
-//     }
-//    };
+exports.fairydoll_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+    res.render('fairydollcreate', { title: 'fairydoll Create'});
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
 
 // Handle building the view for updating a fairydoll.
 // query provides the id
-// exports.fairydoll_update_Page = async function(req, res) {
-//     console.log("update view for item "+req.query.id)
-//     try{
-//     let result = await fairydoll.findById(req.query.id)
-//     res.render('fairydollupdate', { title: 'fairydoll Update', toShow: result });
-//     }
-//     catch(err){
-//     res.status(500)
-//     res.send(`{'error': '${err}'}`);
-//     }
-//    };
+exports.fairydoll_update_Page = async function(req, res) {
+    console.log("update view for id " + req.query.id)
+    try{
+        
+    result = await Fairydoll.findById( req.query.id)
+    res.render('fairydollupdate',
+   { title: 'fairydoll Update', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
+
+   
+exports.fairydoll_view_one_Page = async function(req, res) {
+    console.log("single view for id " + req.query.id)
+    try{
+        
+    result = await Fairydoll.findById( req.query.id)
+    res.render('fairydolldetail',
+   { title: 'fairydoll Detail', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
 
 //- Handle a delete one view with id from query
-// exports.fairydoll_delete_Page = async function(req, res) {
-//     console.log("Delete view for id " + req.query.id)
-//     try{
-//     result = await fairydoll.findById(req.query.id)
-//     res.render('fairydolldelete', { title: 'fairydoll Delete', toShow:
-//    result });
-//     }
-//     catch(err){
-//        res.status(500)
-//        res.send(`{'error': '${err}'}`);
-//     }
-//    };
+exports.fairydoll_delete_Page = async function(req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try{
+    result = await Fairydoll.findById(req.query.id)
+    res.render('fairydolldelete', { title: 'fairydoll Delete', toShow:
+   result });
+    }
+    catch(err){
+       res.status(500)
+       res.send(`{'error': '${err}'}`);
+    }
+   };
--- routes/fairydoll.js
+++ routes/fairydoll.js
@@ -11,12 +11,12 @@ router.get('/', fairydoll_controlers.fairydoll_view_all_Page);
 router.get('/detail', fairydoll_controlers.fairydoll_view_one_Page);
 
 /* GET create fairydoll page */
-// router.get('/create', fairydoll_controlers.fairydoll_create_Page);
+router.get('/create', fairydoll_controlers.fairydoll_create_Page);
 
-// /* GET create update page */
-// router.get('/update', fairydoll_controlers.fairydoll_update_Page);
+/* GET create update page */
+router.get('/update', fairydoll_controlers.fairydoll_update_Page);
 
 // /* GET create fairydoll page */
-// router.get('/delete', fairydoll_controlers.fairydoll_delete_Page);
+router.get('/delete', fairydoll_controlers.fairydoll_delete_Page);
 
 module.exports = router;
